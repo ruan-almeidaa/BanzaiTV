e4520f585e93128f4543e573dca25b01db8cb867
Ruan <ruanalmeidawegner@gmail.com>
2024-07-27 22:32:45 -0300

    style: adicionada paginação e filtros nas tabelas.

--- BanzaiTV/wwwroot/js/site.js
+++ BanzaiTV/wwwroot/js/site.js
@@ -1,4 +1,22 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function() {
+    let table = new DataTable('#tabela', {
+        // Configurações do DataTables
+        paging: true,
+        searching: true,
+        ordering: true,
+        info: true,
+        lengthChange: true,
+        language: {
+            // Personalizar textos e mensagens de interface
+            paginate: {
+                previous: "Anterior",
+                next: "Próximo"
+            },
+            search: "Buscar:",
+            lengthMenu: "Mostrar até _MENU_linhas",
+            info: "Mostrando da linha _START_ até _END_ num total de _TOTAL_ linhas",
+            infoEmpty: "Mostrando 0 a 0 de 0 entradas",
+            emptyTable: "Nenhum dado disponível na tabela"
+        }
+    });
+});
